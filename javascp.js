f0a3c854bbcb97cdf95868d95740e086df21c1a3
NoobProgramCoder <102610279+NoobProgramCoder@users.noreply.github.com>
2022-03-30 08:33:41 +0545

    Create javascp.js

---
+++ javascp.js
@@ -0,0 +1,127 @@
+/*
+function showdate()
+{
+            let day = Date();
+document.getElementById('date').innerHTML = "Today is "+day+" now.";
+}
+
+*/
+
+/*
+var num = prompt("Please enter your amount you want to donate:");
+if (num <= 10)
+{
+    document.write("Sorry but according to your payment choice You can not buy this item.");
+}
+else{
+    document.write("You are welcome please choose our product.");
+}
+*/
+
+/*
+let a = 20;
+let b = 10;
+let sum = a + b;
+document.write(sum);
+console.log(sum);
+
+*/
+// console.log("There is nothin to see here.")
+//alert(sum);
+
+/*
+
+let r = prompt("Please enter the radius()Go to Cocsole to check area):");
+let unit = prompt("Please enter the unit  for your inut:");
+let area = 3.14 * r * r;
+console.log("The area of circle is "+area.toFixed(2) +  unit + ".");
+
+*/
+
+// Loops
+
+/*
+let i =  prompt("Please enter the first number from where you want to start your loop:");
+let j = prompt("Please enter the last number upto which you want loop to stop:");
+alert("Please checkout at console tab by r.clicking in the webpage and inspect button and then the console tab.");
+
+console.log("Loops from "+i+"upto"+j+"is as follows:")
+for ( i ; i<=j; i++)
+{
+console.log(i);
+}
+
+*/
+
+
+
+//Arrays 
+
+
+/*
+const dins=['sun','mon','tue','wed','thu','fri']; 
+// console.log(dins[6]);
+dins.forEach(function(dayz) 
+{
+document.write(dayz+" ");
+}
+);
+dins.splice(2,2,'ant','aant');
+document.write("<br>")
+
+dins.forEach(function(dayz) 
+{
+document.write(dayz+" ");
+}
+);
+
+*/
+
+
+//Functions:
+
+/*
+function prac(){
+
+   // document.write("Sorry no magic.")
+    document.getElementById('magic').innerHTML = "Wait for magic to show up..."
+}
+
+*/
+
+/*
+
+function  interest(){
+let prp = document.getElementById('p').value;
+let tim = document.getElementById('t').value;
+let rate= document.getElementById('r').value;
+let amt= (prp*tim*rate)/100 ; 
+let total = prp+amt; 
+document.getElementById('total').innerHTML = "The total simple interest is " + total+" ." ; 
+
+}
+*/
+
+// Using Objects
+
+/*
+let me = {
+ fname : " Ashim",
+ lname : "Nepal",
+ age : 17,
+ location : "Kathmandu"
+
+};
+console.log(me.fname);
+console.log(me.age);
+
+*/
+
+
+// Other functions in JS
+let val= 2.46;
+let a = Math.round(val);
+let b = Math.pow(8,2);
+console.log(a);
+console.log(b);
+
